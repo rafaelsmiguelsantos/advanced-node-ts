f6c6d98d600b214593022cb5916a29e9a68fb7da
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-06-10 18:24:14 -0300

    test: ensure Service calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  transform: {
-    '\\.ts$': 'ts-jest'
-  },
   moduleNameMapper: {
-    '@/tests(.+)': '<rootDir>/tests/$1',
+    '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
   },
+  testMatch: ['**/*.spec.ts'],
   roots: [
     '<rootDir>/src',
     '<rootDir>/tests'
-  ]
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
 }
--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,16 +1,24 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
+import { LoadUserAccountRepository } from '@/data/contracts/repos/user-account'
 import { FacebookAuthenticationService } from '@/data/services/facebook-authentication/facebook-authentication'
 import { AuthenticationError } from '@/domain/errors/authentication-error'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let loadUserAccountRepository: MockProxy<LoadUserAccountRepository>
   let sut: FacebookAuthenticationService
   const token = 'any_token'
 
   beforeEach(() => {
     loadFacebookUserApi = mock()
-    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    loadFacebookUserApi.loadUser.mockResolvedValue({
+      name: 'any_name',
+      email: 'any_email',
+      facebookId: 'facebook_id'
+    })
+    loadUserAccountRepository = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepository)
   })
   it('should call LoadFacebookUserApi with correct params', async () => {
     await sut.perform({ token })
@@ -25,5 +33,12 @@ describe('FacebookAuthenticationService', () => {
     const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
+  })
+
+  it('should return LoadUserAccountRepository when LoadFacebookUserApi returns data', async () => {
+    await sut.perform({ token })
+
+    expect(loadUserAccountRepository.load).toHaveBeenCalledWith({ email: 'any_email' })
+    expect(loadUserAccountRepository.load).toHaveBeenCalledTimes(1)
   })
 })
