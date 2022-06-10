d32bb0a7987cb68c4750ec60d6e2cf26f7c8729d
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-06-10 18:23:33 -0300

    feat:ensure Service calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- src/data/services/facebook-authentication/facebook-authentication.ts
+++ src/data/services/facebook-authentication/facebook-authentication.ts
@@ -1,11 +1,18 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
+import { LoadUserAccountRepository } from '@/data/contracts/repos/user-account'
 import { AuthenticationError } from '@/domain/errors/authentication-error'
 import { FacebookAuthentication } from '@/domain/features/facebook-authentication'
 
 export class FacebookAuthenticationService {
-  constructor (private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi) { }
+  constructor (
+    private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepository: LoadUserAccountRepository) { }
+
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserByTokenApi.loadUser(params)
+    const fbData = await this.loadFacebookUserByTokenApi.loadUser(params)
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepository.load({ email: fbData.email })
+    }
     return new AuthenticationError()
   }
 }
