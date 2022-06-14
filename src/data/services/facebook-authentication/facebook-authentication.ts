b4a7ae27d1fc0f85ad9e3526bdd87ceff8a00bc9
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-06-14 14:40:27 -0300

    feat: ensure Service calls TokenGenerator with correct params

--- src/data/services/facebook-authentication/facebook-authentication.ts
+++ src/data/services/facebook-authentication/facebook-authentication.ts
@@ -1,5 +1,6 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/data/contracts/repos/user-account'
+import { TokenGenerator } from '@/data/crypto/token'
 import { AuthenticationError } from '@/domain/errors/authentication-error'
 import { FacebookAccount } from '@/domain/models/facebook-account'
 import { FacebookAuthentication } from '@/domain/use-case/facebook-authentication'
@@ -7,14 +8,16 @@ import { FacebookAuthentication } from '@/domain/use-case/facebook-authenticatio
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository) { }
+    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator) { }
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepository.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
-      await this.userAccountRepository.saveWithFacebook(fbAccount)
+      const { id } = await this.userAccountRepository.saveWithFacebook(fbAccount)
+      await this.crypto.generateToken({ key: id })
     }
     return new AuthenticationError()
   }
